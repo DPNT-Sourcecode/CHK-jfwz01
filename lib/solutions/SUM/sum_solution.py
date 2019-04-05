a4e52c273d4831d2b3e1686f38c12024206734e5
sbx_user1051 <sbx_user1051@169.254.163.117>
2019-04-05 23:30:22 +0000

    Fri Apr 05 23:30:22 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,11 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+def sum(x, y):
+    """ Calculates the sum between 2 positive integers.
+    :param x: first number in the sum calculation.
+    :type x: int
+    :param y: second number in the sum calculation.
+    :type y: int
+    :return: sum between x and y
+    """
+    return x + y
+
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,9 @@
-from solutions.SUM import sum_solution
+from lib.solutions.SUM import sum_solution
 
 
-class TestSum():
+class TestSum:
     def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
+        assert sum_solution.sum(1, 2) == 3
+        assert sum_solution.sum(2, 2) == 4
+        assert sum_solution.sum(3, 2) == 5
+
