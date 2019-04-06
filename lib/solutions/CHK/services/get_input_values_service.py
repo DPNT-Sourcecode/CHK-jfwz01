25e9212cb1c3bc19fc322616fd46639494e8271c
sbx_user1051 <sbx_user1051@169.254.144.181>
2019-04-06 15:10:54 +0000

    Sat Apr 06 15:10:54 UTC 2019

--- lib/solutions/CHK/services/get_input_values_service.py
+++ lib/solutions/CHK/services/get_input_values_service.py
@@ -7,6 +7,8 @@ class GetInpuValuesService(object):
     Attributes:
         input_line(str): input that contains the necessary values to perform a checkout.
                          The input line is expected to be: A, B, 3C, A
+    Returns:
+        List containing all the input values. If the input is invalid returns an empty list.
     """
     def __init__(self, input_line):
         self.input_line = input_line
@@ -14,5 +16,6 @@ class GetInpuValuesService(object):
     def call(self):
         split_input_values_regex = re.compile('[0-9]*[A-Z]')
         return split_input_values_regex.findall(self.input_line)
+
 
 
--- test/solution_tests/CHK/services/test_get_input_values_service.py
+++ test/solution_tests/CHK/services/test_get_input_values_service.py
@@ -2,6 +2,13 @@ from lib.solutions.CHK.services.get_input_values_service import GetInpuValuesSer
 
 
 class TestGetInputValuesService:
+    """ Tests the GetInputValuesService when provided with valid and invalid inputs.
+
+    The service should return all the correct values in a list.
+    Example:
+        For a valid input like 'A, 2A, 3B' it should return ['A', '2A', '3B']
+        For an invalid input like '1, 2, 3, 123' it should return []
+    """
     def test_get_input_values_with_valid_input(self):
         valid_input = 'A, 2A, 3B, C, D,'
         expected_return_value = ['A', '2A', '3B', 'C', 'D']
@@ -12,11 +19,12 @@ class TestGetInputValuesService:
         assert expected_return_value == returned_value
 
     def test_get_input_values_with_invalid_input(self):
-        valid_input = '123 123 31312'
-        expected_return_value = -1
+        invalid_input = '1 123 31312'
+        expected_return_value = []
 
-        service = GetInpuValuesService(valid_input)
+        service = GetInpuValuesService(invalid_input)
         returned_value = service.call()
 
         assert expected_return_value == returned_value
+
 
