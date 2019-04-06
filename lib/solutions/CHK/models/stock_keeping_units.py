49985c146dcbc79806b63f7436a2dee14d2184e8
sbx_user1051 <sbx_user1051@169.254.139.253>
2019-04-06 13:41:26 +0000

    Sat Apr 06 13:41:26 UTC 2019

--- lib/solutions/CHK/models/stock_keeping_units.py
+++ lib/solutions/CHK/models/stock_keeping_units.py
@@ -5,9 +5,11 @@ class StockKeepUnits(object):
     Attributes:
         name (str): name of the supermarket item.
         price (int): price of the item.
-        special_offers (dict): different price if you buy X ammount of items. This dict will be {ammount_to_ }
+        special_offers (dict): different price if you buy X ammount of items.
+                               This dict will be {ammount_to_buy: price}
     """
     def __init__(self, name, price, special_offers):
         self.name = name
         self.price = price
         self.special_offers = special_offers
+
