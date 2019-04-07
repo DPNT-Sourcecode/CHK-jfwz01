d1951d285b83d4d69735ec5e8909da2e415fb3e3
sbx_user1051 <sbx_user1051@169.254.75.29>
2019-04-07 22:37:32 +0000

    Sun Apr 07 22:37:32 UTC 2019

--- test/solution_tests/CHK/services/test_calculate_total_checkout_value_service.py
+++ test/solution_tests/CHK/services/test_calculate_total_checkout_value_service.py
@@ -33,7 +33,7 @@ class TestCalculateTotalCheckoutValueService:
         """ Tests calculating the checkout value with single and multiple quantity items.
 
         For the provided mock values should return:
-            200+130+50+45+40+20 = 505
+            200+130+50+45+40+20+40 = 505
         """
         special_offers_item_a = [SpecialOffer(3, 130, None), SpecialOffer(5, 200, None)]
         special_offers_item_b = [SpecialOffer(2, 45, None)]
@@ -52,4 +52,29 @@ class TestCalculateTotalCheckoutValueService:
         returned_value = service.call()
 
         assert returned_value, expected_value
+
+    def test_calculate_total_checkout_value_service_with_free_item_special_offer(self):
+        """ Tests calculating the checkout value with single and multiple quantity items.
+
+        For the provided mock values should return:
+            200+130+50+40+20+80 = 480
+        """
+        special_offers_item_a = [SpecialOffer(3, 130, None), SpecialOffer(5, 200, None)]
+        special_offers_item_b = [SpecialOffer(2, 45, None)]
+        item_a = StockKeepUnit('A', 50, special_offers_item_a)
+        item_b = StockKeepUnit('B', 30, special_offers_item_b)
+        item_c = StockKeepUnit('C', 20, [])
+        item_d = StockKeepUnit('D', 15, [])
+        special_offers_item_e = [SpecialOffer(2, 0, item_b)]
+        item_e = StockKeepUnit('E', 40, special_offers_item_e)
+
+        mock_supermarket = SuperMarket([item_a, item_b, item_c, item_d, item_e])
+        input_items_count = {'A': 9, 'B': 1, 'C': 1, 'D': 1, 'E': 2}
+        expected_value = 520
+
+        service = CalculateTotalCheckoutValueService(input_items_count, mock_supermarket)
+        returned_value = service.call()
+
+        assert returned_value, expected_value
+
 
