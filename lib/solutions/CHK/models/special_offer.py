ef31ad8bb73463c16395b3c815e9c3c7c771a346
sbx_user1051 <sbx_user1051@169.254.75.29>
2019-04-07 22:30:19 +0000

    Sun Apr 07 22:30:19 UTC 2019

--- lib/solutions/CHK/models/special_offer.py
+++ lib/solutions/CHK/models/special_offer.py
@@ -5,7 +5,10 @@ class SpecialOffer:
     Attributes:
         count(int): number of items needed to perform the special offer.
         price(int): new price for the count of items.
+        free_item(<StockKeepingUnit>): the special offer instead of a different price offers a free item.
     """
-    def __init__(self, count, price):
+    def __init__(self, count, price, free_item):
         self.count = count
         self.price = price
+        self.free_item = free_item
+
--- lib/solutions/CHK/services/calculate_total_checkout_value_service.py
+++ lib/solutions/CHK/services/calculate_total_checkout_value_service.py
@@ -21,25 +21,42 @@ class CalculateTotalCheckoutValueService:
             if not item.special_offers:
                 total_checkout_value += item.price * count
             else:
-                total_checkout_value += self._get_item_total_value_with_several_special_offers(item, count)
+                total_checkout_value += self._get_item_total_checkout_value_from_several_special_offers(item, count)
 
         return total_checkout_value
 
-    @staticmethod
-    def _get_item_total_value_with_several_special_offers(item, count):
-        """ Returns the total value needed to pay for a StockKeepingUnit item that has a special offer.
+    def _get_item_total_checkout_value_from_several_special_offers(self, item, count):
+        """ Returns the total checkout value needed to pay for an item that can have several special offers.
+
         :param item: stock keeping unit present in the supermarket.
         :type item: <StockKeepingUnit>
         :param count: total number of times the item is being bought.
         :type count: int
-        :return: total value needed to pay for that ammount of item.
+        :return: total checkout value needed to pay for the `count` of `item`.
         """
-        item_total_value = 0
+        item_total_checkout_value = 0
+        item_count = count
         special_offers = item.get_special_offers_sorted_by_item_count()
 
         for special_offer in special_offers:
-            while count >= special_offer.count:
-                item_total_value += special_offer.price
-                count -= special_offer.count
+            while item_count >= special_offer.count:
+                free_item = special_offer.free_item
 
-        return item_total_value + (count * item.price)
+                if free_item and self._free_item_is_in_checkout_items(free_item):
+                    item_total_checkout_value -= free_item.price
+                else:
+                    item_total_checkout_value += special_offer.price
+                item_count -= special_offer.count
+
+        return item_total_checkout_value + (item_count * item.price)
+
+    def _free_item_is_in_checkout_items(self, free_item):
+        """ Checks if the free item offered in the special offer is part of the checkout items.
+        :param free_item: item offered in the special offer.
+        :type free_item: <StockKeepingUnit>
+        :return: True if it's present in the checkout. False otherwise.
+        """
+        if free_item.name in self.checkout_items_count:
+            return True
+        return False
+
