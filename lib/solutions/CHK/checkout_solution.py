e2770c53f1a6fc12fcf84051d81faa33ba73e8f8
sbx_user1051 <sbx_user1051@169.254.75.29>
2019-04-07 22:36:19 +0000

    Sun Apr 07 22:36:19 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,16 +8,17 @@ from solutions.CHK.handlers.checkout_handler import CheckoutHandler
 # skus = unicode string
 def checkout(skus):
     # create start stock keeping_units
-    special_offers_item_a = [SpecialOffer(3, 130), SpecialOffer(5, 200)]
-    special_offers_item_b = [SpecialOffer(2, 45)]
-    special_offers_item_e = [SpecialOffer(2, 45)]
+    special_offers_item_a = [SpecialOffer(3, 130, None), SpecialOffer(5, 200, None)]
+    special_offers_item_b = [SpecialOffer(2, 45, None)]
     item_a = StockKeepUnit('A', 50, special_offers_item_a)
     item_b = StockKeepUnit('B', 30, special_offers_item_b)
-    item_c = StockKeepUnit('C', 20, {})
-    item_d = StockKeepUnit('D', 15, {})
-    item_e = StockKeepUnit('E', 40, {})
+    item_c = StockKeepUnit('C', 20, [])
+    item_d = StockKeepUnit('D', 15, [])
+    special_offers_item_e = [SpecialOffer(2, 0, item_b)]
+    item_e = StockKeepUnit('E', 40, special_offers_item_e)
 
     # create supermarket
     supermarket = SuperMarket([item_a, item_b, item_c, item_d, item_e])
 
     return CheckoutHandler.calculate(skus, supermarket)
+
