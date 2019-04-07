cba773420c5dd3b5147d126cc9b82e76e9ab3e26
sbx_user1051 <sbx_user1051@169.254.8.53>
2019-04-07 21:47:54 +0000

    Sun Apr 07 21:47:54 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,9 +9,9 @@ from solutions.CHK.handlers.checkout_handler import CheckoutHandler
 def checkout(skus):
     # create start stock keeping_units
     special_offers_item_a = [SpecialOffer(3, 130), SpecialOffer(5, 200)]
-    special_offer_item_b = SpecialOffer(2, 45)
-    item_a = StockKeepUnit('A', 50, special_offer_item_a)
-    item_b = StockKeepUnit('B', 30, special_offer_item_b)
+    special_offers_item_b = [SpecialOffer(2, 45)]
+    item_a = StockKeepUnit('A', 50, special_offers_item_a)
+    item_b = StockKeepUnit('B', 30, special_offers_item_b)
     item_c = StockKeepUnit('C', 20, {})
     item_d = StockKeepUnit('D', 15, {})
     item_d = StockKeepUnit('E', 40, {})
@@ -20,4 +20,5 @@ def checkout(skus):
     supermarket = SuperMarket([item_a, item_b, item_c, item_d])
 
     return CheckoutHandler.calculate(skus, supermarket)
+
 
--- lib/solutions/CHK/models/stock_keeping_unit.py
+++ lib/solutions/CHK/models/stock_keeping_unit.py
@@ -5,10 +5,18 @@ class StockKeepUnit:
     Attributes:
         name (str): name of the supermarket item.
         price (int): price of the item.
-        special_offer (<SpecialOffer>): different price if you buy X ammount of items.
+        special_offers ([<SpecialOffer>]): list of special offers that allows to
+                                           buy X ammount of items at a different price.
     """
     def __init__(self, name, price, special_offers):
         self.name = name
         self.price = price
         self.special_offers = special_offers
+
+    def _get_special_offers_sorted_by_item_count(self):
+        """ Returns the special offers of a StockKeepUnit sorted in descending order of item count.
+        :return: List of <SpecialOffer> sorted in descending order of item count.
+        """
+        return sorted(self.special_offers, key=lambda special_offer: special_offer.count, reverse=True)
+
 
